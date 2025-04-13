1d5d9ed1097b67dac6af9df494c05f704ec869a3
Buchci <dawid.buchci@gmail.com>
2025-04-13 22:45:37 +0200

    Created a login screen

---
+++ Purofessor/Views/Login.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Purofessor.Views
+{
+    /// <summary>
+    /// Logika interakcji dla klasy Login.xaml
+    /// </summary>
+    public partial class Login : Page
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+        private void LoginButton_Click(object sender, RoutedEventArgs e)
+        {
+            string login = LoginTextBox.Text;
+            string haslo = PasswordBox.Password;
+
+            if (login == "admin" && haslo == "1234")
+            {
+                this.NavigationService.Navigate(new Counterpick());
+                MessageBox.Show("Zalogowano pomyślnie!");
+            }
+            else
+            {
+                MessageBox.Show("Nieprawidłowy login lub hasło.");
+            }
+        }
+    }
+}
