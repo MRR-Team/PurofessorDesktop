0c2ba0d8762caaea941e796f527a44e6364d0b37
Buchci <dawid.buchci@gmail.com>
2025-05-24 20:58:58 +0200

    Seperated logout button into a compoment

---
+++ Purofessor/components/PanelCompoments/LogoutButton.xaml.cs
@@ -0,0 +1,32 @@
+﻿using Purofessor.Views;
+using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Purofessor.components.PanelComponents
+{
+    public partial class LogoutButton : UserControl
+    {
+        public LogoutButton()
+        {
+            InitializeComponent();
+        }
+
+        private async void LogoutButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                await App.ApiService.LogoutAsync();
+
+                var loginWindow = new LoginWindow();
+                loginWindow.Show();
+
+                Window.GetWindow(this)?.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Błąd podczas wylogowywania: {ex.Message}");
+            }
+        }
+    }
+}
