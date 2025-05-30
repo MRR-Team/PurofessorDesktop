5b459abc751c46bc332212db1f9c85351d0fea59
Buchci <dawid.buchci@gmail.com>
2025-05-30 22:05:01 +0200

    Added ThemeHelper for setting Themes

---
+++ Purofessor/Helpers/ThemeHelper.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Purofessor.Helpers
+{
+    public static class ThemeHelper
+    {
+        public static void SetTheme(string themeFile)
+        {
+            if (string.IsNullOrWhiteSpace(themeFile))
+            {
+                themeFile = "DarkTheme.xaml";
+            }
+
+            var dict = new ResourceDictionary
+            {
+                Source = new Uri($"Themes/{themeFile}", UriKind.Relative)
+            };
+
+            // Usuń poprzedni motyw
+            var existingThemeDict = Application.Current.Resources.MergedDictionaries
+                .FirstOrDefault(d => d.Source != null && d.Source.OriginalString.StartsWith("Themes/"));
+
+            if (existingThemeDict != null)
+                Application.Current.Resources.MergedDictionaries.Remove(existingThemeDict);
+
+            // Dodaj nowy motyw
+            Application.Current.Resources.MergedDictionaries.Add(dict);
+        }
+    }
+}
