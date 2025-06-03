c5199b19bf88b157aefda320dc051ba3f4410fdb
Buchci <dawid.buchci@gmail.com>
2025-06-03 14:57:23 +0200

    Created TopPanel_Admin

---
+++ Purofessor/components/TopPanel_Admin.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Purofessor.components
+{
+    /// <summary>
+    /// Interaction logic for TopPanel_Admin.xaml
+    /// </summary>
+    public partial class TopPanel_Admin : UserControl
+    {
+        public TopPanel_Admin()
+        {
+            InitializeComponent();
+        }
+    }
+}
