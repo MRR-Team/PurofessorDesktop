fc237779a11ba10ea8cbed4fa55cce465401c2da
Buchci <dawid.buchci@gmail.com>
2025-06-06 23:49:17 +0200

    vs

--- Purofessor/obj/Debug/net8.0-windows/Purofessor.AssemblyInfo.cs
+++ Purofessor/obj/Debug/net8.0-windows/Purofessor.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Purofessor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a62eaef07dafe75812708a6c463dad1fb6721f08")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+979dd834fd60e556a60594b66b59f734c21204bc")]
 [assembly: System.Reflection.AssemblyProductAttribute("Purofessor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Purofessor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
