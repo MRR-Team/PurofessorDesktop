ba4a53bad682fbd54701f0714c092aab43e23739
Buchci <buchc@DESKTOP-GS0K8N4>
2025-06-08 02:27:15 +0200

    Made E2E test for login

---
+++ Purofessor.E2E/LoginTests.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+using FlaUI.Core;
+using FlaUI.UIA3;
+using FlaUI.Core.AutomationElements;
+using Xunit;
+
+namespace Purofessor.E2E
+{
+    public class LoginTests : IDisposable
+    {
+        private readonly Application _app;
+        private readonly AutomationBase _automation;
+
+        public LoginTests()
+        {
+            // Ścieżka do zbudowanej aplikacji
+            var appPath = Path.GetFullPath(@"..\..\..\..\Purofessor\bin\Debug\net8.0-windows\Purofessor.exe");
+
+            _app = Application.Launch(appPath);
+            _automation = new UIA3Automation();
+        }
+
+        [Fact]
+        public void Login_WithValidCredentials_OpensMainWindow()
+        {
+            var window = _app.GetMainWindow(_automation);
+
+            // znajdź pola
+            var loginBox = window.FindFirstDescendant(cf => cf.ByAutomationId("LoginTextBox"))?.AsTextBox();
+            var passwordBox = window.FindFirstDescendant(cf => cf.ByAutomationId("PasswordBox"))?.AsTextBox(); // PasswordBox może być trudny, zależnie od UIA
+
+            var loginButton = window.FindFirstDescendant(cf => cf.ByAutomationId("LoginButton"))?.AsButton();
+
+            Assert.NotNull(loginBox);
+            Assert.NotNull(passwordBox);
+            Assert.NotNull(loginButton);
+
+            // podaj dane
+            loginBox.Enter("SzefSzef@Szef");
+            passwordBox.Enter("SzefSzefSzef");
+
+            loginButton.Invoke();
+
+            // poczekaj na przejście do nowego okna
+            Thread.Sleep(2000);
+
+            var allWindows = _app.GetAllTopLevelWindows(_automation);
+            var mainWindow = allWindows.FirstOrDefault(w => w.Title.Contains("Purofessor"));
+
+            Assert.NotNull(mainWindow);
+        }
+
+        public void Dispose()
+        {
+            _automation.Dispose();
+            _app.Close();
+        }
+    }
+}
---
+++ Purofessor.E2E/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace Purofessor.E2E
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
