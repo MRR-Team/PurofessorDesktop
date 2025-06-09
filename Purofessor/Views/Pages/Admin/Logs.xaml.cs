f65f58e558ceb806e7e66ebc5f9fee5264113e4d
Buchci <dawid.buchci@gmail.com>
2025-06-09 23:07:57 +0200

    Added logs

--- Purofessor/Views/Pages/Admin/Logs.xaml.cs
+++ Purofessor/Views/Pages/Admin/Logs.xaml.cs
@@ -1,17 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using Purofessor.Models;
+using Purofessor.Helpers;
 
 namespace Purofessor.Views.Pages.Admin
 {
@@ -23,6 +16,20 @@ namespace Purofessor.Views.Pages.Admin
         public Logs()
         {
             InitializeComponent();
+            _ = LoadLogsAsync();
+        }
+
+        private async Task LoadLogsAsync()
+        {
+            try
+            {
+                var logs = await ApiService.Instance.Users.GetLogsAsync();
+                LogsDataGrid.ItemsSource = logs; // <- Zmienione z LogsListView na LogsDataGrid
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Błąd wczytywania logów:\n{ex.Message}", "Błąd", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
         }
     }
 }
